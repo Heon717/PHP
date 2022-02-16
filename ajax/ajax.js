7b748d49eb408f55f0c25a64d11fff21db3d6350
Heon <81272630+Heon717@users.noreply.github.com>
2022-02-16 11:56:11 +0900

    Add files via upload

---
+++ ajax/ajax.js
@@ -0,0 +1,9 @@
+$.ajax({
+    type: 'POST',       // http 통신 타입
+    url: '',            // http 통신으로 호출한 url 
+    data: '',           // http 통신 시 같이 보낼 파라미터값
+    datatype: 'JSON',   // http 통신 후 서버에서 받은 데이터의 타입값
+    success: (res) => { // Ajax 통신 성공시 실행 할 함수 ( Ajax 통신 후 서버에서 데이터를 받아 처리하는 부분)
+
+    }
+})
